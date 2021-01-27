7344cb71b1d5f28ec2f0c373984e5eb85cd1a5b3
mohammedBnomr <moohaboy@gmail.com>
2021-01-27 12:14:42 +0300

    Added SVG to login form

--- admin/index.php
+++ admin/index.php
@@ -33,7 +33,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
 ?>
-
+<div class="top-svg">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#007BFF" fill-opacity="1" d="M0,96L60,80C120,64,240,32,360,48C480,64,600,128,720,144C840,160,960,128,1080,144C1200,160,1320,224,1380,256L1440,288L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
+    </svg>
+</div>
 <div class="container login">
 
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" id="contact-form" class="contact-form rounded pt-4 pb-2 px-3" method="POST">
@@ -52,5 +56,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </form>
 
 </div>
-
+<div class="bottom-svg">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#007BFF" fill-opacity="1" d="M0,256L60,261.3C120,267,240,277,360,266.7C480,256,600,224,720,224C840,224,960,256,1080,250.7C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+    </svg>
+</div>
 <?php require_once("includes/templates/footer.php"); ?>
